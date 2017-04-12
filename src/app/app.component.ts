49a17450db8e5045a56d3a0596944a85759d1f51
Edward Louis Burgess <elburgess1@gmail.com>
2017-04-12 11:44:35 -0700

    working through angular 2 tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>
+  Hello {{name}}
+  </h1>
+  <p>Email: {{email}}</p>
+  <p>{{address.street}} {{address.city}} {{address.state}}</p>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Eddie';
+  email = 'elburgess1@gmail.com'
+  address = {
+    street: '25th Avenue South',
+    city: 'Seattle',
+    state: 'WA'
+  }
+}
